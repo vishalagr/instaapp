8a712d713484edad797df9e7cb505888e38558f3
vishal <vishalagr2008@gmail.com>
2015-07-21 20:01:59 +0530

    pg added to push on heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
   	@instagram =[]
+  	# client = Instagram.client(:access_token => current_user.identities.first.accesstoken )
+  	# puts "*********client************#{client.inspect}"
+  	# user = client.user
+  	# puts "*********user************#{client.user}"
+  	#Instagram.create_subscription("user", "http://test.xcard.me")
   	if current_user.identities !=[]
   	  @instagram = Instagram.user_recent_media(current_user.identities.first.uid)
   	end  
